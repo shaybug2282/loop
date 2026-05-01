962568cb547d553cdb9fd758ac83a83311fb892f
ajrom <ajromeo7226@gmail.com>
2026-05-01 11:25:17 -0400

    im unruining it

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const favicon = require('serve-favicon');
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-App.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 root.render(
   <React.StrictMode>
     <App />
